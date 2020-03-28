2b81924da639c330c99a21b1b1da1c7aae1f8b1c
Alexander <montyguide@gmail.com>
2020-03-28 22:01:36 +0500

    5. Конвертер валюты[#243897]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -9,5 +9,11 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(-2, 4, 8, 3);
         System.out.println("Расстояние между двумя точками A1(x1, y1) и A2(x2, y2) = " + result);
+        int a = 6;
+        int b = 6;
+        int c = 6;
+        int p = (a+b+c)/2;
+        double s = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        System.out.println(s);
     }
 }
