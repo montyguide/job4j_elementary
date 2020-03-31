8efd4380cb28f0cf6e926f96533435a0b1a61ed3
montyguide <montyguide@gmail.com>
2020-03-31 07:52:16 +0500

    0. Операции сравнения[#243862]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result1 = first > second; // first больше second?
+                System.out.println(result1);
+
+        boolean result2 = first < second; // first меньше second?
+                System.out.println(result2);
+
+        boolean result3 = first == second; // first равна second?
+                System.out.println(result3);
+    }
+}
