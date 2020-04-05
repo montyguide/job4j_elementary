0f6e0a182a3edc168b57d1e4802025da2cbfc371
montyguide <montyguide@gmail.com>
2020-04-05 23:45:36 +0500

    5.7. Ипотека.[#243939]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -1,1 +1,14 @@
+package ru.job4j.loop;
 
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        amount += ((amount / 100) * percent);
+        while (amount > 0) {
+            amount -= salary;
+            amount += ((amount / 100) * percent);
+            year++;
+        }
+        return year;
+    }
+}
