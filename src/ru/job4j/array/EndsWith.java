fd3fe1feb5572dba7d9674305ea49cb736c86fa7
montyguide <montyguide@gmail.com>
2020-04-13 01:41:50 +0500

    6.4.1. Слово заканчивается с ...[#243877]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = word.length - post.length, j = 0; i < word.length; i++, j++) {
+            if (word[i] == post[j]) {
+                result = true;
+            } else if (word[i] != post[j]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
